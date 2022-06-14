ddc4836a23b549bec0adef468db96c9e7f74fe3a
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-06-14 06:55:10 +0600

    services section complete

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import BottomFooter from './components/Footer/BottomFooter';
 import NewProducts from './components/NewProducts/NewProducts';
+import Services from './components/Services/Services';
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <NewProducts></NewProducts>
+      <Services></Services>
       <Footer></Footer>
       <BottomFooter></BottomFooter>
     </div>
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,7 +1,70 @@
 import React from 'react';
-
+import cashBack from './cashBack.jpg';
+import shipping from './shipping.jpg';
+import support from './support.jpg';
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div
+      style={{
+        padding: '40px 0',
+        width: '80%',
+        marginLeft: '10%',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3,1fr)',
+        gridGap: '20px',
+      }}
+    >
+      <div
+        style={{
+          padding: '30px',
+          textAlign: 'center',
+          border: '1px solid #000',
+          borderRadius: '8px',
+        }}
+      >
+        <img src={shipping} style={{ height: '70px', width: '70px' }} alt="" />
+        <div style={{ fontSize: '20px', fontWeight: 'bold', margin: '15px 0' }}>
+          Free Shipping
+        </div>
+        <div style={{ fontSize: '15px' }}>For Invoice</div>
+        <div style={{ fontSize: '15px' }}>over $1.500</div>
+      </div>
+
+      <div
+        style={{
+          padding: '30px',
+          textAlign: 'center',
+          border: '1px solid #000',
+          borderRadius: '8px',
+        }}
+      >
+        <img src={cashBack} style={{ height: '70px', width: '70px' }} alt="" />
+        <div style={{ fontSize: '20px', fontWeight: 'bold', margin: '15px 0' }}>
+          Cash Back
+        </div>
+        <div style={{ fontSize: '15px' }}>
+          When paying for products <br /> via Dasun Wallet
+        </div>
+      </div>
+
+      <div
+        style={{
+          padding: '30px',
+          textAlign: 'center',
+          border: '1px solid #000',
+          borderRadius: '8px',
+        }}
+      >
+        <img src={support} style={{ height: '70px', width: '70px' }} alt="" />
+        <div style={{ fontSize: '20px', fontWeight: 'bold', margin: '15px 0' }}>
+          24/7 Support
+        </div>
+        <div style={{ fontSize: '15px' }}>
+          When something goes <br /> wrong
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
